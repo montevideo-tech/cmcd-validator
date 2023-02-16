25be82aadc128692d76266c511d6b8c02e9df78b
mcamardon <martinac@qualabs.com>
2023-02-16 17:47:54 -0300

    27-m1-implement-key-value-validator: cretaed keyValueValidator function and every necessary function to validate each key.

---
+++ src/keysValueValidator.js
@@ -0,0 +1,53 @@
+
+import {maxLength,isEncoded,validNrrFormat,validValue,roundToNearest, ignoredKey,isReserved} from './validateFunctions'
+
+//keyValValidator takes as a parameter cmcdJson, which is a javascript object. The function iterates through it validating every key value pair.
+function keyValValidator(cmcdJson,errors){
+    for(var key in cmcdJson){
+        isReserved(errors,key);
+        switch(key){
+            case 'bl':
+                roundToNearest(errors,key,cmcdJson[key],100,'ms');
+                break;
+            case 'cid':
+                maxLength(errors,cmcdJson[key]);
+                break;
+            case 'dl':
+                roundToNearest(errors,key,cmcdJson[key],100,'ms');
+                break;
+            case 'mtp':
+                roundToNearest(errors,key,cmcdJson[key],100,'kbps');
+                break;
+            case 'nor':
+                isEncoded(errors,cmcdJson[key]);
+                break;
+            case 'nrr':
+                validNrrFormat(errors,cmcdJson[key]);
+                break;
+            case 'ot':
+                validValue(errors,cmcdJson[key],['m','a','v','av','i','c','tt','k','o']);
+                break;
+            case 'pr':
+                ignoredKey(errors,key,cmcdJson[key],1);
+                break;
+            case 'rtp':
+                roundToNearest(errors,key,cmcdJson[key],100,'kbps');
+                break;
+            case 'sf':
+                validValue(errors,cmcdJson[key],['d','h','s','o']);
+                break;
+            case 'sid': 
+                maxLength(errors,cmcdJson[key]);
+                break;
+            case 'st':
+                validValue(errors,cmcdJson[key],['v','l']);
+                break;
+            case 'su':
+                ignoredKey(errors,key,cmcdJson[key],false);
+                break;
+            case 'v':
+                ignoredKey(errors,key,cmcdJson[key],1);
+                break;      
+        }
+    }
+}
---
+++ src/validateFunctions.js
@@ -0,0 +1,50 @@
+import {keyTypes} from './constants.js'
+
+export const maxLength=(errors,key,value)=>{
+    if(value.length>64){ 
+        console.log('Invalid value for key \'' + key +'\'. Maximum length is 64 characters.');
+        errors.push(createError('invalid-value',key,value));
+    }
+}
+
+export const isEncoded=(errors,key,value)=>{
+    if(decodeURIComponent(value)===value){
+        console.log('The \'' + key + '\' value must be encoded.');
+        errors.push(createError('parameter-encoding',key,value));
+    }
+}
+
+export const validNrrFormat=(errors,key,value)=>{
+    if(!(/^(\d*-\d*)$/.test(value)) || !(/^\d*-$/.test(value)) || !(/^-\d*$/.test(value))){
+        console.log('The \'' + key + '\' value format is incorrect');
+        errors.push(createError('incorrect-format',key,value));
+    }
+}
+
+export const validValue=(errors,key,value,array)=>{
+    if( !array.includes(value)){
+        console.log('\'' + key + '\'' +' value does not meet the necessary requirements. Must be one of the following values: ' + array + '.');
+        errors.push(createError('invalid-value',key,value)); 
+    }
+}
+
+export const roundToNearest=(errors,key,value,num,unit)=>{
+    if((value%num)!=0){
+        console.log('\''+key-+'\'' + ' value is not rounded to the nearest 100' + unit + '.');
+        errors.push(createError('invalid-value',key,value));
+    }
+}
+
+export const ignoredKey=(errors,key, value,exep)=>{
+    if(value===exep){
+        console.log('The \'' + key +'\' key must not be sent if the value is ' + exep );
+        errors.push(createError('unnecessary-parameter',key,value));
+    }
+}
+
+export const isReserved=(errors,key)=>{
+    if(!(key in keyTypes)){
+        console.log('The key \'' + key + '\' is not reserved.');
+        addError(errors,'unknown-key',key,value);
+    }
+}
