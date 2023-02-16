0dad834ed70a268dda3c5a94fe6b6b5a60314a7b
Camilo Borba <camilob@qualabs.com>
2023-02-16 17:59:15 -0300

    22-m1-implement-query-validator: Double quotes checking has been added

--- src/index.js
+++ src/index.js
@@ -8,4 +8,10 @@ app.listen(PORT);
 
 console.log("Server running on port", PORT);
 
-queryValidator("https://dash.akamaized.net/akamai/bbb_30fps/bbb_30fps_3840x2160_12000k/bbb_30fps_3840x2160_12000k_0.m4v?CMCD=cid%3D%2221cf726cfe3d937b5f974f72bb5bd06a%22%2Cot%3Di%2Csf%3Dd%2Csid%3D%22b248658d-1d1a-4039-91d0-8c08ba597da5%22%2Cst%3Dv%2Csu")
+// queryValidator("https://dash.akamaized.net/akamai/bbb_30fps/bbb_30fps_3840x2160_12000k/bbb_30fps_3840x2160_12000k_0.m4v?CMCD=cid%3D%2221cf726cfe3d937b5f974f72bb5bd06a%22%2Cot%3Di%2Csf%3Dd%2Csid%3D%22b248658d-1d1a-4039-91d0-8c08ba597da5%22%2Cst%3Dv%2Csu")
+
+// const text = "\"21cf726cfe3d937b5f974f72bb5bd06a\""
+// console.log(text)
+// console.log(text.indexOf("\""))
+// console.log(text.lastIndexOf("\""))
+// console.log(text.length)
--- src/queryValidator.js
+++ src/queryValidator.js
@@ -1,49 +1,47 @@
-import { createError } from "./error.js";
+// import { createError } from "./error.js";
 
 export const queryValidator = (queryString) => {
+  const error = [];
+
+  //Check if the query is correct
+  const query = queryString.split("?").pop();
+  console.log("query \n", query);
+  
+  const requests = decodeURIComponent(query).split("CMCD=");
+  requests.shift();
+  console.log("requests \n", requests);
+  
+  if (requests.length > 1) {
+    // error.push(createError("incorrect-format"));
+    // console.error("Error \n", error);
+  } else {
     
-    const error = []
-    //Me quedo con lna info de interes
-    const query = queryString.split("?").pop()
-    console.log("query \n", query)
-    
-    //Chequear si hay mas de un CMCD= y el & por si se agrega algo mas despues
-    const requests = decodeURIComponent(query).split('CMCD=')
-    requests.shift()
-    console.log("requests \n", requests)
-        if(requests.length > 1) {
-            error.push(createError("incorrect-format"))
-            console.error("Error \n", error)
+    //Check 3
+    const values = decodeURIComponent(query).split("CMCD=")[1].split(",");
+    values[values.length - 1] = values[values.length - 1].split("&")[0];
+
+    console.log("values\n", values);
+
+    // Check 1,2
+    for (const val of values) {
+      const [key, value] = val.split("=");
+      // console.log(key)
+
+      //Check 7
+      if (typeof value === "String" && !(value.indexOf("\"") == 0 && value.lastIndexOf("\"") == value.length-1)){
+        // error.push(createError("invalid-value", key, value));
+      }
+
+      if (typeof value === "undefined") {
+        console.log("key bool \n", key);
+
+        //Check 2
+        if (keyTypes[key] !== "Boolean") {
+        //   error.push(createError("wrong-type-value", key, value));
         }
-        else {
-            //Saco el & por si hay uno despues del test
-            const values = decodeURIComponent(query).split('CMCD=')[1].split(',');
-            // console.log("values", values)
-            values[values.length-1] = values[values.length-1].split("&")[0]
-
-            console.log('values\n', values)
-
-            // Separo entre = cada valor de value
-            // Chequear si hay un key = value
-            // Si hay value y es true esta mal
-            // Si no hay value es porque la key tiene que ser bool (fijarme en lo de Martu)
-
-            // console.log(values[5].split('='))
-
-            for(const val of values){
-                const [key, value] = val.split("=")
-                // console.log(key)
-
-                if(typeof(value) === "undefined"){
-                    console.log("key bool \n", key)
-
-                    //chequear en lo que hizo Martu si es un bool, si no reportar error
-                    if (keyTypes[key] != "Boolean"){
-                        error.push(createError("wrong-type-value", key, value))
-                    }
-                }
-            }
-        }
-    
-        return queryString, error
-}
+        
+      }
+    }
+  }
+  return queryString, error;
+};
