9e2478af22f1103a9c48eba817c429695f801a48
Camilo Borba <camilob@qualabs.com>
2023-02-17 15:25:55 -0300

    22-m1-implement-query-validator: keyTypes has been fixed. testQueryValidator has been created and added to index.js. queryValidator has been finished.

--- src/queryValidator.js
+++ src/queryValidator.js
@@ -1,47 +1,58 @@
-// import { createError } from "./error.js";
+import { keyTypes } from "./constants.js";
+import { createError } from "./error.js";
 
 export const queryValidator = (queryString) => {
   const error = [];
 
-  //Check if the query is correct
-  const query = queryString.split("?").pop();
-  console.log("query \n", query);
-  
-  const requests = decodeURIComponent(query).split("CMCD=");
-  requests.shift();
-  console.log("requests \n", requests);
-  
-  if (requests.length > 1) {
-    // error.push(createError("incorrect-format"));
-    // console.error("Error \n", error);
+  // Check if the URL is encoded and if there is something before CMCD request
+  if (decodeURI(queryString) === queryString) {
+    error.push(createError("parameter-encoding"));
   } else {
-    
-    //Check 3
-    const values = decodeURIComponent(query).split("CMCD=")[1].split(",");
-    values[values.length - 1] = values[values.length - 1].split("&")[0];
+    const query = queryString.split("?").pop();
 
-    console.log("values\n", values);
+    //Check if the query is correct
+    const requests = decodeURIComponent(query).split("CMCD=");
+    requests.shift();
 
-    // Check 1,2
-    for (const val of values) {
-      const [key, value] = val.split("=");
-      // console.log(key)
+    if (requests.length > 1) {
+      error.push(createError("incorrect-format"));
+    } else {
+      const values = decodeURIComponent(query).split("CMCD=")[1].split(",");
+      values[values.length - 1] = values[values.length - 1].split("&")[0];
 
-      //Check 7
-      if (typeof value === "String" && !(value.indexOf("\"") == 0 && value.lastIndexOf("\"") == value.length-1)){
-        // error.push(createError("invalid-value", key, value));
-      }
+      // console.log("values\n", values);
 
-      if (typeof value === "undefined") {
-        console.log("key bool \n", key);
+      // Check: key/value is separated by =
+      for (const val of values) {
+        const [key, value] = val.split("=");
 
-        //Check 2
-        if (keyTypes[key] !== "Boolean") {
-        //   error.push(createError("wrong-type-value", key, value));
+        //Check: string require ""
+        if (
+          (keyTypes[key] === "String" &&
+            !(
+              value.indexOf('"') == 0 &&
+              value.lastIndexOf('"') == value.length - 1
+            )) ||
+          (keyTypes[key] === "Token" &&
+            value.indexOf('"') == 0 &&
+            value.lastIndexOf('"') == value.length - 1)
+        ) {
+          error.push(createError("invalid-value", key, value));
         }
-        
+
+        // Check: if the key does not have value it must be a bool
+        if (
+          (typeof value === "undefined" && keyTypes[key] !== "Boolean") ||
+          value === "true"
+        ) {
+          error.push(createError("wrong-type-value", key, value));
+        }
       }
     }
   }
-  return queryString, error;
+
+  return {
+    queryString,
+    error,
+  };
 };
