66bc6aeae7f4a47a206deded068c3047e95f4579
mcamardon <martinac@qualabs.com>
2023-02-21 17:17:07 -0300

    34-m1-implement-json-validator: updated the return statement.

--- src/jsonValidator.js
+++ src/jsonValidator.js
@@ -1,21 +1,17 @@
 import { createError } from './error.js';
 
 export const jsonIsValid = (jsonString, errors) => {
-
   try {
     const checkJson = JSON.parse(jsonString);
   } catch (error) {
     errors.push(createError("invalid-json"));
     return {
       valid : false,
-      jsonString,
-      errors,
-    };
+      errors
+    }
   }
-
   return {
     valid : true,
-    jsonString,
-    errors,
-  };
+    errors
+  }
 }
