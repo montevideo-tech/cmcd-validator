b0cc55fb03c7dd24d93b11772b9c68c347b8243d
Joaquin Bartaburu <joaquinb@qualabs.com>
2023-02-24 15:40:04 -0300

    42-m1-add-rollup: not used import erased

--- rollup.config.js
+++ rollup.config.js
@@ -3,8 +3,6 @@ import nodeResolve from '@rollup/plugin-node-resolve'
 import json from '@rollup/plugin-json'
 import commonjs from '@rollup/plugin-commonjs'
 import babel from '@rollup/plugin-babel'
-import { getBabelOutputPlugin } from '@rollup/plugin-babel';
-
 
 export default [
   {
