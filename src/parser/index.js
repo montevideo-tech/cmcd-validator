a1825037ca7e4d3008916db826a858cafeead535
Camilo Borba <camilob@qualabs.com>
2023-02-24 11:06:57 -0300

    40-m1-implement-a-folder-structure: src/parser/index.js has been fixed to use cmcdTypes

--- src/parser/index.js
+++ src/parser/index.js
@@ -1,13 +1,13 @@
-import { keyTypes } from '../utils/constants.js';
+import { cmcdTypes, keyTypes } from '../utils/constants.js';
 
 export const parseQueryToJson = (queryString) => {
   const values = decodeURIComponent(queryString).split('CMCD=')[1].split('&')[0].split(',');
   const obj = {};
   values.forEach((value) => {
     const [key, val] = value.split('=');
-    if (keyTypes[key] === 'Integer') {
+    if (keyTypes[key] === cmcdTypes.number) {
       obj[key] = parseInt(val, 10);
-    } else if (keyTypes[key] === 'Boolean') {
+    } else if (keyTypes[key] === cmcdTypes.boolean) {
       obj[key] = !(val === 'false');
     } else {
       obj[key] = val.replaceAll('\'', '');
