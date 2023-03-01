8159095fbf13b5cedb72f6ab007df1144530b054
adriansqualabs <adrians@qualabs.com>
2023-03-01 17:41:15 -0300

    50-m1-implement-end-to-end-json-checking: We add more CMCD tests and we found some issues that need to be fixed

--- src/inputValidator/jsonValidator.js
+++ src/inputValidator/jsonValidator.js
@@ -3,12 +3,37 @@ import { createError } from '../utils/error.js';
 
 const jsonIsValid = (jsonString, errors) => {
   let valid = true;
+  const keyvalue = jsonString.split(",");
+  const keys = [];
+  keyvalue.forEach((key) => {
+    keys.push(key.split(":")[0]);
+  })
+  //console.log(keys);
+  if ((new Set(keys)).size !== keys.length) {
+    errors.push(createError(errorTypes.duplicateKey));
+    return false;
+  }
   try {
-    JSON.parse(jsonString);
+    const obj = JSON.parse(jsonString);
+    // Check if there's double key
+    Object.keys(obj).forEach((key) => {
+
+      if ((keyTypes[key] === cmcdTypes.number && checkQuotes(obj[key]))
+      ) {
+        valid = false;
+        error.push(createError(errorTypes.wrongTypeValue, key, value));
+        //return false;
+      }
+
+    })
+  
+
   } catch (error) {
     errors.push(createError(errorTypes.invalidJson));
     valid = false;
   }
+
+  
   return valid;
 };
 
--- src/tests/index.js
+++ src/tests/index.js
@@ -1,13 +1,13 @@
-import { testQueryValidator, testJsonValidator } from './inputValidator/index.js';
-import { testCMCDQueryValidator } from './CMCDQueryValidator/index.js';
+// import { testQueryValidator, testJsonValidator } from './inputValidator/index.js';
+// import { testCMCDQueryValidator } from './CMCDQueryValidator/index.js';
 import testCMCDJsonValidator from './CMCDJsonValidator/testCMCDJsonValidator.js';
 
-console.log('--------- Test queryValidator ---------');
-testQueryValidator();
-console.log('\n');
-console.log('--------- Test jsonValidator---------');
-testJsonValidator();
+// console.log('--------- Test queryValidator ---------');
+// testQueryValidator();
+// console.log('\n');
+// console.log('--------- Test jsonValidator---------');
+// testJsonValidator();
 console.log('--------- Test jsonValidator ---------');
 testCMCDJsonValidator();
- console.log('--------- Test CMCDQueryValidator ---------');
- testCMCDQueryValidator();
+//  console.log('--------- Test CMCDQueryValidator ---------');
+//  testCMCDQueryValidator();
