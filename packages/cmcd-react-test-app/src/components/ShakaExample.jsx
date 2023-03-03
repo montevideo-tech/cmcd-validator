643bd3448ddc85be8d6639f58341d9d4cdcae71a
Tomas Arrivillaga <tarri@dolby.com>
2023-03-03 16:37:57 -0300

    56: adds mux.js to support other types of streams

--- packages/cmcd-react-test-app/src/components/ShakaExample.jsx
+++ packages/cmcd-react-test-app/src/components/ShakaExample.jsx
@@ -1,19 +1,23 @@
 import React, { useEffect, useRef, useState } from "react";
 import ShakaPlayer from "shaka-player-react";
+import muxjs from "mux.js";
 import "shaka-player/dist/controls.css";
 import "./ShakaExample.css";
 import { DataWindow } from "./DataWindow";
 import { ValidatorView } from "./ValidatorView";
-import { CMCDQueryValidator } from '@montevideo-tech/cmcd-validator'
+import { CMCDQueryValidator } from "@montevideo-tech/cmcd-validator";
 
 export function ShakaExample() {
+  window.muxjs = muxjs;
   const controllerRef = useRef(null);
   const [newData, setNewData] = useState([{}]);
   const [validatorOutput, setValidatorOutput] = useState("");
-  const [url, setUrl] = useState("https://demo.unified-streaming.com/k8s/live/stable/scte35-no-splicing.isml/.mpd");
+  const [url, setUrl] = useState(
+    "https://demo.unified-streaming.com/k8s/live/stable/scte35-no-splicing.isml/.mpd"
+  );
   const handleInput = (input) => {
-    setUrl(input.target.value)
-  }
+    setUrl(input.target.value);
+  };
 
   const onButtonClick = () => {
     const {
@@ -35,15 +39,17 @@ export function ShakaExample() {
       const newUris = [{}];
       const urisToAdd = [...new Set(request.uris)];
       for (const uri of urisToAdd) {
-          newUris.push({ url: uri, result: CMCDQueryValidator(uri) });
+        newUris.push({ url: uri, result: CMCDQueryValidator(uri) });
       }
       setNewData(newUris);
     });
     async function loadAsset() {
       // Load an asset.
-      await player.load(
-        url
-      );
+      try {
+        await player.load(url);
+      } catch (error) {
+        console.log(error);
+      }
       // Trigger play.
       videoElement.play();
     }
@@ -53,18 +59,27 @@ export function ShakaExample() {
   return (
     <>
       <div className="mb-4 contet input-group d-flex justify-content-center">
-        <input className="text ms-2" placeholder="Manifest URL" onChange={handleInput}></input>
-        <button className="btn btn-primary" onClick={onButtonClick}>Start Shaka</button>
+        <input
+          className="text ms-2"
+          placeholder="Manifest URL"
+          onChange={handleInput}
+        ></input>
+        <button className="btn btn-primary" onClick={onButtonClick}>
+          Start Shaka
+        </button>
       </div>
       <div className="row">
         <div className="col-6">
           <div className="col-12 mb-4">
             <ShakaPlayer ref={controllerRef} />
           </div>
-          <ValidatorView output={validatorOutput}/>
-        </div >
+          <ValidatorView output={validatorOutput} />
+        </div>
         <div className="col-6">
-          <DataWindow newData={newData} setValidatorOutput={setValidatorOutput}/>
+          <DataWindow
+            newData={newData}
+            setValidatorOutput={setValidatorOutput}
+          />
         </div>
       </div>
     </>
