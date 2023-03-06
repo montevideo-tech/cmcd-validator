58beb2bb1785526be7bfc423486062aece5b9900
Camilo Borba <camilob@qualabs.com>
2023-03-06 11:54:23 -0300

    68-m1-fix-headervalidator: test has been uncomment

--- packages/cmcd-validator-library/src/tests/index.js
+++ packages/cmcd-validator-library/src/tests/index.js
@@ -1,16 +1,16 @@
 import { testCMCDQueryValidator, testCMCDHeaderValidator, testCMCDJsonValidator } from './CMCDValidator/index.js';
 import { testQueryValidator, testJsonValidator, testHeaderValidator } from './inputValidator/index.js';
 
-// console.log('--------- Test queryValidator ---------');
-// testQueryValidator();
-// console.log('\n');
-// console.log('------------Test Header validator------------');
-// testHeaderValidator();
-// console.log('--------- Test jsonValidator ---------');
-// testJsonValidator();
-// console.log('--------- Test CMCDQueryValidator ---------');
-// testCMCDQueryValidator();
+console.log('--------- Test queryValidator ---------');
+testQueryValidator();
+console.log('\n');
+console.log('------------Test Header validator------------');
+testHeaderValidator();
+console.log('--------- Test jsonValidator ---------');
+testJsonValidator();
+console.log('--------- Test CMCDQueryValidator ---------');
+testCMCDQueryValidator();
 console.log('------------Test CMCDHeaderValidator------------');
 testCMCDHeaderValidator();
-// console.log('--------- Test CMCDJsonValidator ---------');
-// testCMCDJsonValidator();
+console.log('--------- Test CMCDJsonValidator ---------');
+testCMCDJsonValidator();
