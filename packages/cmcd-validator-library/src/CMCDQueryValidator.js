c5df0dbc0b60e1ad38d0082701344c7666c45afb
Rspillere <rspillere@gmail.com>
2023-03-08 12:40:09 -0300

    86-m1.5: checkConfig.js added, constants added

--- packages/cmcd-validator-library/src/CMCDQueryValidator.js
+++ packages/cmcd-validator-library/src/CMCDQueryValidator.js
@@ -1,12 +1,19 @@
 import { queryValidator } from './inputValidator/index.js';
 import { keyValValidator } from './keyValueValidator/index.js';
 import { parseQueryToJson } from './parser/index.js';
+import { checkConfig } from './utils/checkConfig.js';
 import { createOutput } from './utils/output.js';
 
-const CMCDQueryValidator = (query) => {
+const CMCDQueryValidator = (query, config) => {
   const errors = [];
   const rawData = query;
-
+  // check config
+  if (config) {
+    const checkConfiguration = checkConfig(config);
+    if (!checkConfiguration.valid) {
+      return checkConfiguration;
+    }
+  }
   // Check query
   const valid = queryValidator(query, errors);
 
