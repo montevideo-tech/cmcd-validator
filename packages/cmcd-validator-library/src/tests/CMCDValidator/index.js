58376889376f71482a247adcaccc4b19e51ae23a
Camilo Borba <camilob@qualabs.com>
2023-03-09 16:56:48 -0300

    95-m15-change-simple-tests-and-use-jest-on-json: Json test has been added using Jest

--- packages/cmcd-validator-library/src/tests/CMCDValidator/index.js
+++ packages/cmcd-validator-library/src/tests/CMCDValidator/index.js
@@ -1,3 +1,2 @@
-export { default as testCMCDHeaderValidator } from './testCMCDHeaderValidator.js';
 export { default as testCMCDQueryValidator } from './testCMCDQueryValidator.js';
 export { default as testCMCDJsonValidator } from './testCMCDJsonValidator.js';
--- packages/cmcd-validator-library/src/tests/index.js
+++ packages/cmcd-validator-library/src/tests/index.js
@@ -1,4 +1,4 @@
-import { testCMCDQueryValidator, testCMCDHeaderValidator, testCMCDJsonValidator } from './CMCDValidator/index.js';
+import { testCMCDQueryValidator, testCMCDJsonValidator } from './CMCDValidator/index.js';
 import { testQueryValidator, testJsonValidator, testHeaderValidator } from './inputValidator/index.js';
 
 // console.log('--------- Test queryValidator ---------');
@@ -10,7 +10,5 @@ import { testQueryValidator, testJsonValidator, testHeaderValidator } from './in
 // testJsonValidator();
 // console.log('--------- Test CMCDQueryValidator ---------');
 // testCMCDQueryValidator();
-console.log('------------Test CMCDHeaderValidator------------');
-testCMCDHeaderValidator();
-// console.log('--------- Test CMCDJsonValidator ---------');
-// testCMCDJsonValidator();
+console.log('--------- Test CMCDJsonValidator ---------');
+testCMCDJsonValidator();
