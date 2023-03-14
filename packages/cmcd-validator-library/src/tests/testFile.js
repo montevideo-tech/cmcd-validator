b28838ee37228fd3ea3d3e1bd67f02d079a4a5d5
Rspillere <rspillere@gmail.com>
2023-03-14 12:48:43 -0300

    100-m1.5: new checks added to queryValidator function

--- packages/cmcd-validator-library/src/tests/testFile.js
+++ packages/cmcd-validator-library/src/tests/testFile.js
@@ -1,22 +1,33 @@
 import { CMCDQueryValidator } from '../index.js';
 
-
-const urlI3 = /[-a-zA-Z0-9@:%._\+~#=]{1,256}\.[a-zA-Z0-9()]{1,6}\b([-a-zA-Z0-9()@:%_\+.~#?&//=]*)?/gi;
-const urlI4 = /^(?:([A-Za-z]+):)?(\/{0,3})([0-9.\-A-Za-z]+)(?::(\d+))?(?:\/([^?#]*))?(?:\?([^#]*))?(?:#(.*))?$/
-
-const  ampersandVerify= (query) => {
-  const regex = /\?(.+)(?=CMCD)/;
-  const match = query.match(regex);
-  if (match && !match[1].includes('&')) {
-    return false;
-  } else {
-    return true;
-  }
-}
-const query = "https://dash.akamaized.net/akamai/bbb_30fps/bbb_30fps_3840x2160_12000k/bbb_30fps_3840x2160_12000k_0.m4v?CMCD=cid%3D%2221cf726cfe3d937b5f974f72bb5bd06a%22%2Cot%3Di%2Csf%3Dd%2Csid%3D%22b248658d-1d1a-4039-91d0-8c08ba597da5%22%2Cst%3Dv%2Csu"
-
-const match = query.match(urlI4);
-
-// console.log(ampersandVerify(query));
-
-console.log(match);
+const query = {
+  description:
+    "Test: Correct Query with configuration using customKey and specificKey",
+  query:
+    "https://dash.akamaized.net/akamai/bbb_30fps/bbb_30fps_3840x2160_12000k/bbb_30fps_3840x2160_12000k_0.m4v?CMCD=qualabs-br%3Dfalse%2Ccid%3D%2221cf726cfe3d937b5f974f72bb5bd06a%22%2Cot%3Di%2Csf%3Dd%2Csid%3D%22b248658d-1d1a-4039-91d0-8c08ba597da5%22%2Cst%3Dv%2Csu",
+  config: {
+    customKey: [
+      {
+        key: "qualabs-br",
+        type: "boolean",
+        header: "CMCD-Object",
+      },
+    ],
+    specificKey: ["br", "sid"],
+  },
+  output: {
+    valid: true,
+    errors: [],
+    parsedData: {
+      cid: "21cf726cfe3d937b5f974f72bb5bd06a",
+      ot: "i",
+      "qualabs-br": false,
+      sf: "d",
+      sid: "b248658d-1d1a-4039-91d0-8c08ba597da5",
+      st: "v",
+      su: true,
+    },
+    rawData:
+      "https://dash.akamaized.net/akamai/bbb_30fps/bbb_30fps_3840x2160_12000k/bbb_30fps_3840x2160_12000k_0.m4v?CMCD=qualabs-br%3Dfalse%2Ccid%3D%2221cf726cfe3d937b5f974f72bb5bd06a%22%2Cot%3Di%2Csf%3Dd%2Csid%3D%22b248658d-1d1a-4039-91d0-8c08ba597da5%22%2Cst%3Dv%2Csu",
+  },
+};
