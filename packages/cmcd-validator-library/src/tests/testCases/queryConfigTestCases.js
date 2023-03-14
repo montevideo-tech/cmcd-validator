421aa0ac40bc30e0296d976b9a2110d1e65d5282
Rspillere <rspillere@gmail.com>
2023-03-14 16:44:00 -0300

    86-m1.5: checkConfig improved, added more test cases

--- packages/cmcd-validator-library/src/tests/testCases/queryConfigTestCases.js
+++ packages/cmcd-validator-library/src/tests/testCases/queryConfigTestCases.js
@@ -342,4 +342,85 @@ export const queryConfigTestCases = [
       rawData: 'https://dash.akamaized.net/akamai/bbb_30fps/bbb_30fps_3840x2160_12000k/bbb_30fps_3840x2160_12000k_0.m4v?CMCD=qualabs-br%3Dtrue%2Ccid%3D%2221cf726cfe3d937b5f974f72bb5bd06a%22%2Cot%3Di%2Csf%3Dd%2Csid%3D%22b248658d-1d1a-4039-91d0-8c08ba597da5%22%2Cst%3Dv%2Csu'
     },
   },
+  {
+    description: "Test: Incorrect Query with configuration using customKey with a wrong key name #1",
+    query:
+      "https://dash.akamaized.net/akamai/bbb_30fps/bbb_30fps_3840x2160_12000k/bbb_30fps_3840x2160_12000k_0.m4v?CMCD=qualabs-br%3Dfalse%2Ccid%3D%2221cf726cfe3d937b5f974f72bb5bd06a%22%2Cot%3Di%2Csf%3Dd%2Csid%3D%22b248658d-1d1a-4039-91d0-8c08ba597da5%22%2Cst%3Dv%2Csu",
+    config: {
+      customKey: [
+        {
+          key: "--qualabs-br--",
+          type: "boolean",
+        },
+      ],
+    },
+    output: {
+      valid: false,
+      errors: [
+        {
+          type: 'invalid-custom-key',
+          key: '--qualabs-br--',
+          value: undefined,
+          description: 'Custom key names MUST carry a hyphenated prefix'
+        }
+      ],
+      warnings: [],
+      parsedData: undefined,
+      rawData: 'https://dash.akamaized.net/akamai/bbb_30fps/bbb_30fps_3840x2160_12000k/bbb_30fps_3840x2160_12000k_0.m4v?CMCD=qualabs-br%3Dfalse%2Ccid%3D%2221cf726cfe3d937b5f974f72bb5bd06a%22%2Cot%3Di%2Csf%3Dd%2Csid%3D%22b248658d-1d1a-4039-91d0-8c08ba597da5%22%2Cst%3Dv%2Csu'
+    },
+  },
+  {
+    description: "Test: Incorrect Query with configuration using customKey with a wrong key name #2",
+    query:
+      "https://dash.akamaized.net/akamai/bbb_30fps/bbb_30fps_3840x2160_12000k/bbb_30fps_3840x2160_12000k_0.m4v?CMCD=qualabs-br%3Dfalse%2Ccid%3D%2221cf726cfe3d937b5f974f72bb5bd06a%22%2Cot%3Di%2Csf%3Dd%2Csid%3D%22b248658d-1d1a-4039-91d0-8c08ba597da5%22%2Cst%3Dv%2Csu",
+    config: {
+      customKey: [
+        {
+          key: "-qualabs-",
+          type: "boolean",
+        },
+      ],
+    },
+    output: {
+      valid: false,
+      errors: [
+        {
+          type: 'invalid-custom-key',
+          key: '-qualabs-',
+          value: undefined,
+          description: 'Custom key names MUST carry a hyphenated prefix'
+        }
+      ],
+      warnings: [],
+      parsedData: undefined,
+      rawData: 'https://dash.akamaized.net/akamai/bbb_30fps/bbb_30fps_3840x2160_12000k/bbb_30fps_3840x2160_12000k_0.m4v?CMCD=qualabs-br%3Dfalse%2Ccid%3D%2221cf726cfe3d937b5f974f72bb5bd06a%22%2Cot%3Di%2Csf%3Dd%2Csid%3D%22b248658d-1d1a-4039-91d0-8c08ba597da5%22%2Cst%3Dv%2Csu'
+    },
+  },
+  {
+    description: "Test: Incorrect Query with configuration using customKey with a wrong key name #3",
+    query:
+      "https://dash.akamaized.net/akamai/bbb_30fps/bbb_30fps_3840x2160_12000k/bbb_30fps_3840x2160_12000k_0.m4v?CMCD=qualabs-br%3Dfalse%2Ccid%3D%2221cf726cfe3d937b5f974f72bb5bd06a%22%2Cot%3Di%2Csf%3Dd%2Csid%3D%22b248658d-1d1a-4039-91d0-8c08ba597da5%22%2Cst%3Dv%2Csu",
+    config: {
+      customKey: [
+        {
+          key: "-",
+          type: "boolean",
+        },
+      ],
+    },
+    output: {
+      valid: false,
+      errors: [
+        {
+          type: 'invalid-custom-key',
+          key: '-',
+          value: undefined,
+          description: 'Custom key names MUST carry a hyphenated prefix'
+        }
+      ],
+      warnings: [],
+      parsedData: undefined,
+      rawData: 'https://dash.akamaized.net/akamai/bbb_30fps/bbb_30fps_3840x2160_12000k/bbb_30fps_3840x2160_12000k_0.m4v?CMCD=qualabs-br%3Dfalse%2Ccid%3D%2221cf726cfe3d937b5f974f72bb5bd06a%22%2Cot%3Di%2Csf%3Dd%2Csid%3D%22b248658d-1d1a-4039-91d0-8c08ba597da5%22%2Cst%3Dv%2Csu'
+    },
+  },    
 ];
