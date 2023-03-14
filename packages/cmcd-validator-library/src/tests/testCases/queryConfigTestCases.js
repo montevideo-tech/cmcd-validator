50b3b171e9c2c7078f12dbf7a1710ed078f2b1bd
Rspillere <rspillere@gmail.com>
2023-03-14 13:53:08 -0300

    86-m1.5: new test added

--- packages/cmcd-validator-library/src/tests/testCases/queryConfigTestCases.js
+++ packages/cmcd-validator-library/src/tests/testCases/queryConfigTestCases.js
@@ -315,4 +315,31 @@ export const queryConfigTestCases = [
         "https://dash.akamaized.net/akamai/bbb_30fps/bbb_30fps_3840x2160_12000k/bbb_30fps_3840x2160_12000k_0.m4v?CMCD=qualabs-br%3Dfalse%2Ccid%3D%2221cf726cfe3d937b5f974f72bb5bd06a%22%2Cot%3Di%2Csf%3Dd%2Csid%3D%22b248658d-1d1a-4039-91d0-8c08ba597da5%22%2Cst%3Dv%2Csu",
     },
   },
+  {
+    description: "Test: Incorrect Query with configuration using only customKey sending true value",
+    query:
+      "https://dash.akamaized.net/akamai/bbb_30fps/bbb_30fps_3840x2160_12000k/bbb_30fps_3840x2160_12000k_0.m4v?CMCD=qualabs-br%3Dtrue%2Ccid%3D%2221cf726cfe3d937b5f974f72bb5bd06a%22%2Cot%3Di%2Csf%3Dd%2Csid%3D%22b248658d-1d1a-4039-91d0-8c08ba597da5%22%2Cst%3Dv%2Csu",
+    config: {
+      customKey: [
+        {
+          key: "qualabs-br",
+          type: "boolean",
+        },
+      ],
+    },
+    output: {
+      valid: false,
+      errors: [
+        {
+          type: 'wrong-type-value',
+          key: 'qualabs-br',
+          value: 'true',
+          description: 'Value type is incorrect'
+        }
+      ],
+      warnings: [],
+      parsedData: undefined,
+      rawData: 'https://dash.akamaized.net/akamai/bbb_30fps/bbb_30fps_3840x2160_12000k/bbb_30fps_3840x2160_12000k_0.m4v?CMCD=qualabs-br%3Dtrue%2Ccid%3D%2221cf726cfe3d937b5f974f72bb5bd06a%22%2Cot%3Di%2Csf%3Dd%2Csid%3D%22b248658d-1d1a-4039-91d0-8c08ba597da5%22%2Cst%3Dv%2Csu'
+    },
+  },
 ];
--- packages/cmcd-validator-library/src/tests/testFile.js
+++ packages/cmcd-validator-library/src/tests/testFile.js
@@ -1,23 +1,28 @@
 import { CMCDQueryValidator } from '../index.js';
 
 const query = {
-  description:
-    "Test: Correct Query with configuration using customKey and specificKey",
+  description: "Test: Incorrect Query with configuration using only customKey sending true value",
   query:
-    "https://dash.akamaized.net/akamai/bbb_30fps/bbb_30fps_3840x2160_12000k/bbb_30fps_3840x2160_12000k_0.m4v?CMCD=qualabs-br%3Dfalse%2Ccid%3D%2221cf726cfe3d937b5f974f72bb5bd06a%22%2Cot%3Di%2Csf%3Dd%2Csid%3D%22b248658d-1d1a-4039-91d0-8c08ba597da5%22%2Cst%3Dv%2Csu",
+    "https://dash.akamaized.net/akamai/bbb_30fps/bbb_30fps_3840x2160_12000k/bbb_30fps_3840x2160_12000k_0.m4v?CMCD=qualabs-br%3Dtrue%2Ccid%3D%2221cf726cfe3d937b5f974f72bb5bd06a%22%2Cot%3Di%2Csf%3Dd%2Csid%3D%22b248658d-1d1a-4039-91d0-8c08ba597da5%22%2Cst%3Dv%2Csu",
   config: {
     customKey: [
       {
         key: "qualabs-br",
         type: "boolean",
-        header: "CMCD-Object",
       },
     ],
-    specificKey: ["br", "sid"],
   },
   output: {
     valid: true,
     errors: [],
+    warnings: [
+      {
+        type: 'no-alphabetical-order',
+        key: undefined,
+        value: undefined,
+        description: 'Keys are not arranged alphabetically'
+      }
+    ],
     parsedData: {
       cid: "21cf726cfe3d937b5f974f72bb5bd06a",
       ot: "i",
@@ -28,7 +33,7 @@ const query = {
       su: true,
     },
     rawData:
-      "https://dash.akamaized.net/akamai/bbb_30fps/bbb_30fps_3840x2160_12000k/bbb_30fps_3840x2160_12000k_0.m4v?CMCD=qualabs-br%3Dfalse%2Ccid%3D%2221cf726cfe3d937b5f974f72bb5bd06a%22%2Cot%3Di%2Csf%3Dd%2Csid%3D%22b248658d-1d1a-4039-91d0-8c08ba597da5%22%2Cst%3Dv%2Csu",
+      "https://dash.akamaized.net/akamai/bbb_30fps/bbb_30fps_3840x2160_12000k/bbb_30fps_3840x2160_12000k_0.m4v?CMCD=qualabs-br%3Dtrue%2Ccid%3D%2221cf726cfe3d937b5f974f72bb5bd06a%22%2Cot%3Di%2Csf%3Dd%2Csid%3D%22b248658d-1d1a-4039-91d0-8c08ba597da5%22%2Cst%3Dv%2Csu",
   },
 };
 
