c4df64ac810f7659ba2b02fdbf492addae13d31c
Rspillere <rspillere@gmail.com>
2023-03-15 15:09:44 -0300

    86-m1.5: deleted error in custom Heade because is not in the standard

--- packages/cmcd-validator-library/src/utils/constants.js
+++ packages/cmcd-validator-library/src/utils/constants.js
@@ -44,7 +44,6 @@ export const errorTypes = {
   wrongCustomType: 'wrong-custom-type',
   invalidCustomKey: 'invalid-custom-key',
   unknownSpecificKey: 'unknown-specific-key',
-  wrongCustomHeader: 'wrong-custom-header',
 };
 
 export const errorDescription = {
@@ -65,7 +64,6 @@ export const errorDescription = {
   wrongCustomType: 'The type does not mach with CMCD types',
   invalidCustomKey: 'Custom key names MUST carry a hyphenated prefix',
   unknownSpecificKey: 'The key does not mach with CMCD keys',
-  wrongCustomHeader: 'The header does not mach with CMCD headers',
 };
 
 export const cmcdHeader = {
