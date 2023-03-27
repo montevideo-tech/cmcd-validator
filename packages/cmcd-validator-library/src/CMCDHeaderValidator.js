7e3be5f3abfdd51c147884061767b2f5010e4cdd
Camilo Borba <camilob@qualabs.com>
2023-03-27 16:36:48 -0300

    114-m15-add-warning-to-the-test-test-correct-query-with-configuration-setting-two-specifickeys-but-providing-just-one-in-the-query: eslint issue has been fixed

--- packages/cmcd-validator-library/src/CMCDHeaderValidator.js
+++ packages/cmcd-validator-library/src/CMCDHeaderValidator.js
@@ -1,10 +1,10 @@
+import { v4 as uuidv4 } from 'uuid';
 import { headerValidator } from './inputValidator/index.js';
 import { keyValValidator } from './keyValueValidator/index.js';
 import { parseHeaderToJson } from './parser/index.js';
 import { createOutput } from './utils/output.js';
 import { keyTypes } from './utils/constants.js';
 import { logger } from './logger.js';
-import { v4 as uuidv4 } from 'uuid';
 
 const CMCDHeaderValidator = (header, warningFlag = true) => {
   const errors = [];
