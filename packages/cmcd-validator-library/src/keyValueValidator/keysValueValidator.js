9ce04680d5c0768bc3aec349514fb1aaf71ca361
adriansqualabs <adrians@qualabs.com>
2023-03-31 15:20:27 -0300

    139-m15-add-config-to-cmcdheadervalidator: check github actions problem

--- packages/cmcd-validator-library/src/keyValueValidator/keysValueValidator.js
+++ packages/cmcd-validator-library/src/keyValueValidator/keysValueValidator.js
@@ -13,7 +13,7 @@ const keyValValidator = (cmcdJson, errors, warnings, config, extendedKeyTypes, w
   if (warningFlag === true) {
     checkSidIsPresent(cmcdJson, warnings);
   }
-
+  // test
   Object.keys(cmcdJson).forEach((key) => {
     // Check if we received a configuration
     if (config?.specificKey && !config.specificKey?.includes(key)) {
