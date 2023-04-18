9d110a43f8eae7907ffe57dadda91968498c92bc
joaquinbch <joaquinb@qualabs.com>
2023-04-18 09:55:40 -0300

    fix: dash multiple request fixed

--- packages/cmcd-validator-demo-app/src/components/Players/Dash/Dash.jsx
+++ packages/cmcd-validator-demo-app/src/components/Players/Dash/Dash.jsx
@@ -5,35 +5,48 @@ import { CMCDQueryValidator } from "@montevideo-tech/cmcd-validator";
 
 function DashPlayer({dispatchReqList, manifestURI }) {
   const videoRef = useRef(null);
+  const playerRef = useRef(null);
+
+  useEffect(() => {
+    if(playerRef.current)
+      playerRef.current.attachSource(manifestURI);
+  }, [manifestURI])
 
   useEffect(() => {
     
-    var CMCD_MODE_QUERY = 'query';
-    const player = dashjs.MediaPlayer().create();
-    player.initialize(videoRef.current, manifestURI, true);
-    player.updateSettings({
+    if (videoRef.current) {
+      const CMCD_MODE_QUERY = 'query';
+      const video = videoRef.current;
+
+      playerRef.current = dashjs.MediaPlayer().create();
+
+      playerRef.current.initialize(video, manifestURI, true);
+      playerRef.current.attachView(video);
+      playerRef.current.updateSettings({
         streaming: {
-            cmcd: {
-                enabled: true,
-                 mode: CMCD_MODE_QUERY,
-            }
+          cmcd: {
+            enabled: true,
+            mode: CMCD_MODE_QUERY,
+          }
         }
-    });
-    var origOpen = XMLHttpRequest.prototype.open;
-    XMLHttpRequest.prototype.open = function(method, url) {
-      console.log(url);
-      dispatchReqList({type: 'saveQuery' , payload: { url: url, result: CMCDQueryValidator(url) }})
-      origOpen.apply(this, arguments);
+      });
+
+      var origOpen = XMLHttpRequest.prototype.open;
+      console.log('request intercept event initialized')
+      XMLHttpRequest.prototype.open = function(method, url) {
+        console.log(url);
+        dispatchReqList({type: 'saveQuery' , payload: { url: url, result: CMCDQueryValidator(url) }})
+        origOpen.apply(this, arguments);
+      };
+    }
+
+    return () => {
+      if (playerRef.current) {
+        playerRef.current.destroy();
+        playerRef.current = null;
+      }
     };
-    // player.on(dashjs.MediaPlayer.events.FRAGMENT_LOADING_COMPLETED, function(e) {
-    //   console.log(e.request.url)
-    //   dispatchReqList({type: 'saveQuery' , payload: { url: e.request.url, result: CMCDQueryValidator(e.request.url) }})
-    // });
-    // player.on(dashjs.MediaPlayer.events.MANIFEST_LOADED, function(e) {
-    //   console.log(e.data.url)
-    //   dispatchReqList({type: 'saveQuery' , payload: { url: e.data.url, result: CMCDQueryValidator(e.data.url) }})
-    // });
-  }, [manifestURI]);
+  }, []);
 
   return (
     <video
