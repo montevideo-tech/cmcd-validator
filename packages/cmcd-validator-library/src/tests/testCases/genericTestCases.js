7d0fc4ccaa94a0bce0dbd3ec89146542c3221249
JoaquinBCh <joaquinb@qualabs.com>
2023-05-16 11:13:25 -0300

    error added when mtp equals 0

--- packages/cmcd-validator-library/src/tests/testCases/genericTestCases.js
+++ packages/cmcd-validator-library/src/tests/testCases/genericTestCases.js
@@ -2498,4 +2498,79 @@ sec-ch-ua-platform: "Linux"`,
       },
     },
   },
+  {
+    description: 'Incorrect query with mtp = 0',
+    query: 'https://dash.akamaized.net/akamai/bbb_30fps/bbb_30fps_3840x2160_12000k/bbb_30fps_3840x2160_12000k_0.m4v?CMCD=bl%3D21300%2Cbr%3D3200%2Cbs%2Ccid%3D%22faec5fc2-ac30-11ea-bb37-0242ac130002%22%2Cd%3D4004%2Cdl%3D18500%2Cmtp%3D0%2Cnor%3D%22..%252F300kbps%252Ftrack.m4v%22%2Cnrr%3D%2212323-48763%22%2Cot%3Dv%2Cpr%3D1.08%2Crtp%3D12000%2Csf%3Dd%2Csid%3D%226e2fb550-c457-11e9-bb97-0800200c9a66%22%2Cst%3Dv%2Csu%2Ctb%3D6000',
+    header: `GET /akamai/bbb_30fps/bbb_a64k/bbb_a64k_10.m4a HTTP/1.1
+Accept: */*
+Accept-Encoding: gzip, deflate, br
+Accept-Language: es-ES,es;q=0.9
+CMCD-Request: bl=21300,dl=18500,mtp=0,nor="..%2F300kbps%2Ftrack.m4v",nrr="12323-48763",su
+CMCD-Object: br=3200,d=4004,ot=v,tb=6000
+CMCD-Status: bs,rtp=12000
+CMCD-Session: cid="faec5fc2-ac30-11ea-bb37-0242ac130002",pr=1.08,sf=d,sid="6e2fb550-c457-11e9-bb97-0800200c9a66",st=v
+Connection: keep-alive
+Host: dash.akamaized.net
+Origin: https://reference.dashif.org
+Referer: https://reference.dashif.org/
+Sec-Fetch-Dest: empty
+Sec-Fetch-Mode: cors
+Sec-Fetch-Site: cross-site
+User-Agent: Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/109.0.0.0 Safari/537.36
+sec-ch-ua: "Not_A Brand";v="99", "Google Chrome";v="109", "Chromium";v="109"
+sec-ch-ua-mobile: ?0
+sec-ch-ua-platform: "Linux"`,
+    json: `
+    {
+        "bl": 21300,
+        "br": 3200,
+        "bs": true,
+        "cid": "faec5fc2-ac30-11ea-bb37-0242ac130002",
+        "d": 4004,
+        "dl": 18500,
+        "mtp": 0,
+        "nor": "..%2F300kbps%2Ftrack.m4v",
+        "nrr": "12323-48763",
+        "ot": "v",
+        "pr": 1.08,
+        "rtp": 12000,
+        "sf": "d",
+        "sid": "6e2fb550-c457-11e9-bb97-0800200c9a66",
+        "st": "v",
+        "su": true,
+        "tb": 6000
+    }`,
+    output:
+    {
+      valid: false,
+      errors: [
+        {
+          type: 'unnecessary-key',
+          key: 'mtp',
+          value: 0,
+          description: 'The mtp key should not be sent if the value is 0',
+        }
+      ],
+      warnings: [],
+      parsedData: {
+        bl: 21300,
+        br: 3200,
+        bs: true,
+        cid: 'faec5fc2-ac30-11ea-bb37-0242ac130002',
+        d: 4004,
+        dl: 18500,
+        mtp: 0,
+        nor: '..%2F300kbps%2Ftrack.m4v',
+        nrr: '12323-48763',
+        ot: 'v',
+        pr: 1.08,
+        rtp: 12000,
+        sf: 'd',
+        sid: '6e2fb550-c457-11e9-bb97-0800200c9a66',
+        st: 'v',
+        su: true,
+        tb: 6000,
+      },
+    },
+  },
 ];
