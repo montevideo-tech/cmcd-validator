96c7c6374fe27ebe3ef6c603b0ac02dc0dcf7498
coatesjuan <juanco@qualabs.com>
2023-07-13 16:46:05 -0300

    update videojs and plugin

--- packages/cmcd-validator-demo-app/src/components/Players/VideoJs/VideoJs.jsx
+++ packages/cmcd-validator-demo-app/src/components/Players/VideoJs/VideoJs.jsx
@@ -16,16 +16,11 @@ export const VideoJS = (props) => {
       controls: false,
       responsive: true,
       plugins: {cmcd: {}},
-      fluid: true,
-      
-      sources: [{
-        src: manifestURI
-      }]
-
+      fluid: true
     }
     // Make sure Video.js player is only initialized once
     if (!playerRef.current) {
-      // The Video.js player needs to be _inside_ the component el for React 18 Strict Mode. 
+      // The Video.js player needs to be _inside_ the component elgi for React 18 Strict Mode. 
       const videoElement = document.createElement("video-js");
       
       videoElement.classList.add('vjs-big-play-centered');
@@ -33,17 +28,15 @@ export const VideoJS = (props) => {
       
       const player = playerRef.current = videojs(videoElement, options);
       
-      player.cmcd();
-      
       player.on('ready', () => {player.controls(true);});
+      
       // You could update an existing player in the `else` block here
       // on prop change, for example:
     } else {
       const player = playerRef.current;
       
       player.autoplay(options.autoplay);
-      player.src(options.sources);
-      player.cmcd();      
+      player.src(manifestURI);    
     }
   }, [videoRef, manifestURI]);
 
